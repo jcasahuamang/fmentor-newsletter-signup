8fc7562299dd14f26d38bb1595c33b8563814c5c
jcasahuamang <jcasahuamang@gmail.com>
2025-06-29 16:23:13 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Frontend Mentor - Newsletter sign-up form with success message solution
+
+This is a solution to the [Newsletter sign-up form with success message challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/newsletter-signup-form-with-success-message-3FC1AZbNrv). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [Links](#links)
+- [Author](#author)
+
+## Overview
+
+### Links
+
+- Solution URL: [Add solution URL here](https://jcasahuamang.github.io/fmentor-newsletter-signup.git)
+- Live Site URL: [Add live site URL here](https://jcasahuamang.github.io/fmentor-newsletter-signup)
+
+## Author
+
+- Frontend Mentor - [@yourusername](https://www.frontendmentor.io/profile/jcasahuamang)
---
+++ assets/js/main.js
@@ -0,0 +1,58 @@
+const btnSubscribe = document.getElementById("btnSubscribe");
+const btnDismiss = document.getElementById("btnDismiss");
+const signupForm = document.getElementById("form");
+
+const container = document.querySelector(".container");
+const confirmation = document.querySelector(".confirmation");
+const confirmationEmail = document.querySelector(".confirmation-email");
+
+// select the email form field message element
+const messageLabel = document.querySelector(".form-input-label p");        
+const inputEmail = document.querySelector(".form-input input");
+
+function handleSubmit(e) {
+  e.preventDefault() // prevent the default behaviour
+  
+  const formDataEntries = new FormData(signupForm).entries();
+  const { email } = Object.fromEntries(formDataEntries);
+
+   const emailErrorMessage = validateEmail(email);
+
+   if (emailErrorMessage) {
+    messageLabel.innerText = emailErrorMessage;
+    inputEmail.classList.add('error-input');
+
+    }
+  else{
+    inputEmail.classList.remove('error-input');
+
+    container.classList.toggle('container-active');
+    confirmation.classList.toggle('confirmation-active');
+
+    confirmationEmail.innerText = email;
+    messageLabel.innerText = "";
+    inputEmail.value="";
+
+  }
+
+}
+
+
+signupForm.addEventListener('submit', handleSubmit);
+
+btnDismiss.addEventListener("click", function () {
+    container.classList.toggle('container-active');
+    confirmation.classList.toggle('confirmation-active');
+
+  });
+
+
+function validateEmail(email) {
+  if (!email) return 'Email is required';
+
+  const isValidEmail = /^\S+@\S+$/g
+  if (!isValidEmail.test(email)) {
+    return 'Please enter a valid email';
+  }
+  return '';
+}
